d0038ea561c716c87d223f9ff5647a632031f62b
jeongyong <jeongyong95@gmail.com>
2022-05-27 10:29:21 +0900

    [DOCUMENT-0000] indent 4

--- src/main/java/com/joojeongyong/document/api/controller/FileController.java
+++ src/main/java/com/joojeongyong/document/api/controller/FileController.java
@@ -20,23 +20,25 @@ import java.util.List;
 @RestController
 public class FileController {
 
-  private final FileService fileService;
+    private final FileService fileService;
 
-  @GetMapping
-  public ResponseEntity<List<String>> getFileIdList() {
-    return ResponseEntity.ok(Lists.newArrayList());
-  }
+    @GetMapping
+    public ResponseEntity<List<String>> getFileIdList() {
+        return ResponseEntity.ok(Lists.newArrayList());
+    }
 
-  @GetMapping("/{fileId}")
-  public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) throws IOException {
-    InputStreamResource resource = new InputStreamResource(fileService.downloadFile());
-    return ResponseEntity.status(HttpStatus.OK)
-        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"big-size.zip\"")
-        .contentType(MediaType.APPLICATION_OCTET_STREAM)
-        .contentLength(2_844_757_475L) // contentLength가 헤더에 명시되어야 브라우저에서 다운받을 총 크기를 안내할 수 있다. stream에서 크기를 구하면 스트림을 태워서 읽어버린다.
-        .body(resource);
-  }
+    @GetMapping("/{fileId}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) throws IOException {
+        InputStreamResource resource = new InputStreamResource(fileService.downloadFile());
+        return ResponseEntity.status(HttpStatus.OK)
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"big-size.zip\"")
+            .contentType(MediaType.APPLICATION_OCTET_STREAM)
+            .contentLength(
+                2_844_757_475L) // contentLength가 헤더에 명시되어야 브라우저에서 다운받을 총 크기를 안내할 수 있다. stream에서 크기를 구하면
+            // 스트림을 태워서 읽어버린다.
+            .body(resource);
+    }
 
-  @PostMapping
-  public void uploadFile(@RequestBody MultipartFile file) {}
+    @PostMapping
+    public void uploadFile(@RequestBody MultipartFile file) {}
 }

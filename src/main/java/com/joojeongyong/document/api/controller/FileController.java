7dae547cfb2e96b5c1ec8b429d8c7eabab0569e0
jeongyong <jeongyong95@gmail.com>
2022-05-27 10:27:14 +0900

    [DOCUMENT-3] 대용량 파일 다운로드 실험

--- src/main/java/com/joojeongyong/document/api/controller/FileController.java
+++ src/main/java/com/joojeongyong/document/api/controller/FileController.java
@@ -3,11 +3,16 @@ package com.joojeongyong.document.api.controller;
 import com.google.common.collect.Lists;
 import com.joojeongyong.document.domain.file.FileService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -23,8 +28,13 @@ public class FileController {
   }
 
   @GetMapping("/{fileId}")
-  public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) {
-    return ResponseEntity.ok(null);
+  public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) throws IOException {
+    InputStreamResource resource = new InputStreamResource(fileService.downloadFile());
+    return ResponseEntity.status(HttpStatus.OK)
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"big-size.zip\"")
+        .contentType(MediaType.APPLICATION_OCTET_STREAM)
+        .contentLength(2_844_757_475L) // contentLength가 헤더에 명시되어야 브라우저에서 다운받을 총 크기를 안내할 수 있다. stream에서 크기를 구하면 스트림을 태워서 읽어버린다.
+        .body(resource);
   }
 
   @PostMapping
--- src/main/java/com/joojeongyong/document/domain/file/FileService.java
+++ src/main/java/com/joojeongyong/document/domain/file/FileService.java
@@ -2,6 +2,17 @@ package com.joojeongyong.document.domain.file;
 
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+
 @Service
 public class FileService {
+
+    public InputStream downloadFile() {
+        try {
+            File file = new File("standardlogos_2022.zip");
+            return new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
